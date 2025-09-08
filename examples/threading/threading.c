39c54f77cfd0787877c82740c02c704490a89438
dinanli <fyzx_ldn@outlook.com>
2025-09-08 22:19:38 +0200

    Finish assignment 4 part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,24 +10,48 @@
 
 void* threadfunc(void* thread_param)
 {
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+	
+	thread_func_args->thread_complete_success = false;
+	usleep(thread_func_args->wait_to_obtain_ms*1000);
+	
+	if (pthread_mutex_lock(thread_func_args->mutex) != 0) {
+		ERROR_LOG("Failed to retrieve mutex");
+		return thread_func_args;
+	}
 
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+	usleep(thread_func_args->wait_to_release_ms*1000);
+	
+	if (pthread_mutex_unlock(thread_func_args->mutex) != 0) {
+		ERROR_LOG("Failed to release mutex");
+		return thread_func_args;
+	}
+
+	thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
-    /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
-     * using threadfunc() as entry point.
-     *
-     * return true if successful.
-     *
-     * See implementation details in threading.h file comment block
-     */
+    struct thread_data *args = malloc(sizeof(struct thread_data));
+
+	if (args != NULL) {
+		args->mutex = mutex;
+		args->wait_to_obtain_ms=wait_to_obtain_ms;
+		args->wait_to_release_ms=wait_to_release_ms;
+		args->thread_complete_success=false;
+		
+		int pth = pthread_create(thread, NULL, threadfunc, args);
+		
+		if (pth != 0) {
+			ERROR_LOG("Failed to create thread");
+			free(args);
+			return false;
+		}
+		return true;
+	}
+	ERROR_LOG("Failed to allocate for arguments");
     return false;
 }
 
